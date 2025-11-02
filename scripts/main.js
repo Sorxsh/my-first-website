dcc692ae1bcaa857f8b00e9ad41fcf07a9af8f80
Soroosh Fayazi <83946507+Sorxsh@users.noreply.github.com>
2025-11-02 11:18:57 +0330

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,42 @@
+const listItems = document.querySelectorAll("li");
+
+function toggleDone(e) {
+  if (!e.target.className) {
+    e.target.className = "done";
+  } else {
+    e.target.className = "";
+  }
+}
+
+listItems.forEach((item) => {
+  item.addEventListener("click", toggleDone);
+});
+const myImage = document.querySelector("img");
+myImage.addEventListener("click", () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/Guitar_icon.png") {
+    myImage.setAttribute("src", "images/photo-02.avif");
+  } else {
+    myImage.setAttribute("src", "images/Guitar_icon.png");
+  }
+});
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+   if (!myName) {
+    setUserName();
+  } else {
+  localStorage.setItem("name", myName);
+  myHeading.textContent = `Mozilla is cool, ${myName}`;
+  }
+}
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `Guitars are cool, ${storedName}`;
+}
+myButton.addEventListener("click", () => {
+  setUserName();
+});
